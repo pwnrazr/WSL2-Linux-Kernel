91d017fd3ad6cbfdfb42fbe1dcd10e72ca3c298f
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-09-02 19:20:22 +0800

    builder: back to gcc
    i cant boot for some reason

--- builder.sh
+++ builder.sh
@@ -2,45 +2,11 @@
 
 cpus=`expr $(nproc --all)`
 
-CT_BIN=/home/pwnrazr/dev-stuff/aosp-clang-15/bin/
+#CT_BIN=/home/pwnrazr/dev-stuff/aosp-clang-15/bin/
 
 make wsl_defconfig \
-	PATH=${CT_BIN}:${PATH} \
 	O=out \
-	-j${cpus} \
-        AR="llvm-ar" \
-        AS="llvm-as" \
-        NM="llvm-nm" \
-        STRIP="llvm-strip" \
-        OBJCOPY="llvm-objcopy" \
-        OBJDUMP="llvm-objdump" \
-        OBJSIZE="llvm-size" \
-        READELF="llvm-readelf" \
-        LD="ld.lld" \
-        LDLLD="ld.lld" \
-        HOSTCC="clang" \
-        HOSTCXX="clang++" \
-        LLVM=1 \
-        LLVM_IAS=1 \
-        CC="ccache clang" \
-        CXX="ccache clang++"
+	-j${cpus}
 
 make O=out \
-	PATH=${CT_BIN}:${PATH} \
-	-j${cpus} \
-        AR="llvm-ar" \
-        AS="llvm-as" \
-        NM="llvm-nm" \
-        STRIP="llvm-strip" \
-        OBJCOPY="llvm-objcopy" \
-        OBJDUMP="llvm-objdump" \
-        OBJSIZE="llvm-size" \
-        READELF="llvm-readelf" \
-        LD="ld.lld" \
-        LDLLD="ld.lld" \
-        HOSTCC="clang" \
-        HOSTCXX="clang++" \
-        LLVM=1 \
-        LLVM_IAS=1 \
-        CC="ccache clang" \
-        CXX="ccache clang++"
+	-j${cpus}
--- wsl_menuconfig.sh
+++ wsl_menuconfig.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
 cpus=`expr $(nproc --all)`
-CT_BIN=/home/pwnrazr/dev-stuff/aosp-clang-15/bin/
+#CT_BIN=/home/pwnrazr/dev-stuff/aosp-clang-15/bin/
 
 make wsl_defconfig \
-	PATH=${CT_BIN}:${PATH} \
         O=out \
         -j${cpus} \
-        AR="llvm-ar" \
-        AS="llvm-as" \
-        NM="llvm-nm" \
-        STRIP="llvm-strip" \
-        OBJCOPY="llvm-objcopy" \
-        OBJDUMP="llvm-objdump" \
-        OBJSIZE="llvm-size" \
-        READELF="llvm-readelf" \
-        LD="ld.lld" \
-        LDLLD="ld.lld" \
-        HOSTCC="clang" \
-        HOSTCXX="clang++" \
-        CC="clang" \
-        CXX="clang++" \
-	LLVM=1 \
-	LLVM_IAS=1 \
 	menuconfig
